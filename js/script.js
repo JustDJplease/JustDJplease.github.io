bb7c18fabcb49b81e5261b0c380b264028360bee
Floris <floris.jolink@gmail.com>
2021-12-26 22:38:45 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,13 +35,9 @@ $(document).ready(function() {
 
    // --> Load new data into the content section.
    function loadContent(id) {
-      console.log("Started loading a new page");
       $("#content").hide().load("content/" + id + ".html").fadeIn('250');
-      console.log("Finished loading a new page");
       currentlyViewing = id;
-      
-      // Show breadcrumbs after 1 second.
-      setTimeout(function(){updateBreadCrumbs()}, 5000);
+      updateBreadCrumbs();
    }
 
    // ---------------------------------- //
